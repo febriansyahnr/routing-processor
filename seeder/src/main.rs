3de4373e034e75875a1f841d7ef4aa3ea27ce8cd
febriannr <febriansyah.rayiid@paper.id>
2024-11-18 03:41:27 +0700

    update seeder

--- seeder/src/main.rs
+++ seeder/src/main.rs
@@ -1,4 +1,5 @@
 use core::prelude::*;
+use prelude::TSeeder;
 
 mod processor_seeder;
 mod prelude;
@@ -7,18 +8,21 @@ mod transfer_config_seeder;
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Run Seeders");
-    // let conf = core::config::get_config();
-    // let poll = core::utils::connection::get_mysql_pool(&conf.database_url).await?;
-
-    // Processor Seeder
-    // processor_seeder::ProcessorSeeder::new("processor_seeder", &poll)
-    // .execute()
-    // .await?;
-
-    // Transfer Config Seeder
-    // transfer_config_seeder::TransferConfigSeeder::new("transfer_config_seeder", &poll)
-    // .execute()
-    // .await?;
+    let should_run = false; // set true to run seeder
+    if should_run {
+        let conf = core::config::get_config();
+        let poll = core::utils::connection::get_mysql_pool(&conf.database_url).await?;
+    
+        // Processor Seeder
+        processor_seeder::ProcessorSeeder::new("processor_seeder", &poll)
+        .execute()
+        .await?;
+    
+        // Transfer Config Seeder
+        transfer_config_seeder::TransferConfigSeeder::new("transfer_config_seeder", &poll)
+        .execute()
+        .await?;
+    }
 
     Ok(())
 }
